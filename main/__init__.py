89ee9ae17809e7979e9d4282711f9cb15f02e6b4
verufan <110793296+verufan@users.noreply.github.com>
2023-07-20 19:21:06 +0700

    main __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("20047995", default=None, cast=int)
+API_HASH = config("585b93fd73853e393c564b80c81ea550", default=None)
+BOT_TOKEN = config("6384237561:AAHdOivkwPps2GllgEWHtBGcZbgdHvL5xro", default=None)
+SESSION = config("BAEx6HsAndO86Z1z9Zs1VBJy4_zp9CYuMNCPVKIkL12LAV2pfhKBDzGJaG-u_Pgq72W8Rd7iYK24PBDfqF0iR7MoHpHwqeoU6cWw5iMqdAO0l4rFqklQZ44kGf56AB2yq57YyYTu7qKqZ6aA_2ZakWPdqnKygDcIBb_Ptr9UZa1RndmGj2DYabywsRXIw7WQ39g5f-6CVScD2hzMyTolK6AlrGnj7UKNpHow96iwbr8CnOu_ZRPQXNIq4fvtCtIJ1O_9DJONVrh9nrub0YYKefmqgHQg5aaYKaMVaGU7HekM08wNkgTMULbOkTLJr5NQ3XVlzQgXQeG00oZAJcAbGaH2QWIXywAAAAFxB_KYAA", default=None)
+FORCESUB = config("colongcontenbot", default=None)
+AUTH = config("6191313560", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
